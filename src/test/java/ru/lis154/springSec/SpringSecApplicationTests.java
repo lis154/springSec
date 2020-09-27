2f4cfb4ed33e73ca72f20927b5e63bf685a96ca3
lis154 <lis154@mail.ru>
2020-09-27 23:09:09 +0300

    Initial commit

---
+++ src/test/java/ru/lis154/springSec/SpringSecApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lis154.springSec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
